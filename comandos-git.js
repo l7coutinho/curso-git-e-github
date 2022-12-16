77354009d9b653d53e58cf6b308eb71fa7cd9a61
Lucas Coutinho <lg7coutinho@gmail.com>
2022-12-16 13:16:58 -0300

    atualização de comandos git-github

---
+++ comandos-git.js
@@ -0,0 +1,11 @@
+// git init - inicia o projeto
+// git clone - clona o projeto do diretorio remoto
+// git add - serve para adicionar pastas ao repositório
+// git commit - Armazena o conteúdo atual do índice em um novo commit, juntamente com uma mensagem de registro do usuário que descreve as mudanças. Se usa o commit depois de já ter feito o git add, para fazer o commit:
+// git status - A principal ferramenta utilizada para determinar quais arquivos estão em quais estados é o comando:
+// git rm - Para remover um arquivo do Git, você tem que removê-lo dos arquivos que estão sendo monitorados
+// git branch "name" -  Isso cria uma nova "pasta" para o mesmo commit em que você está no momento.
+// git checkout "name" - serve para alterar a pasta do commit
+// git merge "name" - serve para puxar atualizações feitas em outro branch.
+// git pull - serve para atualizar seu diretório a partir do diretorio remoto. Atualizando local
+// git push - é o comando em que você transfere commits a partir do seu repositório local para um repositório remoto.
